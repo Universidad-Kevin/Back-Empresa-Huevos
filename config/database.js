6089d16942daef368e5dbec5a237438e9405ef3a
Kevin <kevinanthonytenoriorojas159@gmail.com>
2025-11-18 21:01:48 -0500

    back arreglado

--- config/database.js
+++ config/database.js
@@ -4,15 +4,13 @@ dotenv.config();
 // Configuración de la conexión a MySQL
 const dbConfig = {
   host: process.env.DB_HOST,
+  port: 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0,
-  ssl: {
-    rejectUnauthorized: true,
-  },
+  queueLimit: 0
 };
 
 // Crear pool de conexiones
